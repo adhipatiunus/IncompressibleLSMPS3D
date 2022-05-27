1823809340851de05ec39a9762cf74840ee1a7c8
adhipatiunus <adhipatiunus@gmail.com>
2022-05-27 13:06:32 +0700

    improved lsmps

--- LSMPS.py
+++ LSMPS.py
@@ -32,29 +32,39 @@ def LSMPS(particle, R_e, typ):
 def calculate_derivative(particle, R_e, neighbor_list, typ):
     N = len(particle.x)
     b_data = [np.array([])] * N
-    EtaDx   = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDy   = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDz   = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDxx  = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDxy  = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDxz  = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDyy  = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDyz  = sparse.csr_matrix((N, N), dtype=np.float64)
-    EtaDzz  = sparse.csr_matrix((N, N), dtype=np.float64)
-    index   = sparse.csr_matrix((N, N), dtype=np.float64)
 
     if typ == 'x' or typ == 'y' or typ == 'z':
         index_lsmps = [particle.index[i] for i in range(N) if particle.boundary[i] == False]
+        if typ == 'x':
+            EtaDx   = sparse.csr_matrix((N, N), dtype=np.float64)
+            EtaDxx  = sparse.csr_matrix((N, N), dtype=np.float64)
+        elif typ == 'y':
+            EtaDy   = sparse.csr_matrix((N, N), dtype=np.float64)
+            EtaDyy  = sparse.csr_matrix((N, N), dtype=np.float64)
+        else:
+            EtaDz   = sparse.csr_matrix((N, N), dtype=np.float64)
+            EtaDzz  = sparse.csr_matrix((N, N), dtype=np.float64)
     else:
         index_lsmps = particle.index
+        EtaDx   = sparse.csr_matrix((N, N), dtype=np.float64)
+        EtaDy   = sparse.csr_matrix((N, N), dtype=np.float64)
+        EtaDz   = sparse.csr_matrix((N, N), dtype=np.float64)
+        EtaDxx  = sparse.csr_matrix((N, N), dtype=np.float64)
+        EtaDyy  = sparse.csr_matrix((N, N), dtype=np.float64)
+        EtaDzz  = sparse.csr_matrix((N, N), dtype=np.float64)
     #index_lsmps = particle.index
     
     #index_lsmps = [particle.index[i] for i in range(len(particle.x)) if particle.boundary[i] == False]
 
     for i in index_lsmps:
-        H_rs = np.zeros((10,10))
-        M = np.zeros((10,10))
-        P = np.zeros((10,1))
+        if typ == 'x' or typ == 'y' or typ == 'z':
+            H_rs = np.zeros((3, 3))
+            M = np.zeros((3, 3))
+            P = np.zeros((3, 1))
+        else:
+            H_rs = np.zeros((7, 7))
+            M = np.zeros((7, 7))
+            P = np.zeros((7, 1))
         b_temp = [np.array([])] * len(neighbor_list[i])
         
         print('Calculating derivative for particle ' + str(i) + '/' + str(N))
@@ -70,18 +80,21 @@ def calculate_derivative(particle, R_e, neighbor_list, typ):
         y_i = particle.y[idx_i]
         z_i = particle.z[idx_i]
         R_i = R_e * Li
-                
+        
         H_rs[0, 0] = 1
-        H_rs[1, 1] = Li**-1 # Dx
-        H_rs[2, 2] = Li**-1 # Dy
-        H_rs[3, 3] = Li**-1 # Dz
-        H_rs[4, 4] = 2 * Li**-2 # Dxx
-        H_rs[5, 5] = Li**-2 # Dxy
-        H_rs[6, 6] = Li**-2 # Dxz
-        H_rs[7, 7] = 2 * Li**-2 #Dyy
-        H_rs[8, 8] = Li**-2 # Dyz
-        H_rs[9, 9] = 2 * Li**-2 # Dzz
-                
+        
+        if typ == 'x' or typ == 'y' or typ == 'z':
+            H_rs[1, 1] = Li**-1 # Dx / Dy / Dz
+            H_rs[2, 2] = 2 * Li**-2 # Dxx / Dyy/ Dzz
+            
+        else:
+            H_rs[1, 1] = Li**-1 # Dx
+            H_rs[2, 2] = Li**-1 # Dy
+            H_rs[3, 3] = Li**-1 # Dz
+            H_rs[4, 4] = 2 * Li**-2 # Dxx
+            H_rs[5, 5] = 2 * Li**-2 #Dyy
+            H_rs[6, 6] = 2 * Li**-2 # Dzz
+        
         for j in range(len(neighbor_idx)):
             idx_j = neighbor_idx[j]
             x_j = particle.x[idx_j]
@@ -101,16 +114,25 @@ def calculate_derivative(particle, R_e, neighbor_list, typ):
             p_z = z_ij / Li
             
             P[0, 0] = 1.0
-            P[1, 0] = p_x # Dx
-            P[2, 0] = p_y # Dy
-            P[3, 0] = p_z # Dz
-            P[4, 0] = p_x**2 # Dxx
-            P[5, 0] = p_x * p_y # Dxy
-            P[6, 0] = p_x * p_z # Dxz
-            P[7, 0] = p_y**2 # Dyy
-            P[8, 0] = p_y * p_z # Dyz
-            P[9, 0] = p_z**2 # Dzz
             
+            if typ == 'x':
+                P[1, 0] = p_x # Dx
+                P[2, 0] = p_x**2 # Dxx
+                
+            elif typ == 'y':
+                P[1, 0] = p_y # Dy
+                P[2, 0] = p_y**2 # Dyy
+            elif typ == 'z':
+                P[1, 0] = p_z # Dz
+                P[2, 0] = p_z**2 # Dzz
+            else:
+                P[1, 0] = p_x # Dx
+                P[2, 0] = p_y # Dy
+                P[3, 0] = p_z # Dz
+                P[4, 0] = p_x**2 # Dxx
+                P[5, 0] = p_y**2 # Dyy
+                P[6, 0] = p_z**2 # Dzz
+                       
             if r_ij < R_ij:
                 w_ij = (1 - r_ij/R_ij)**2
             else:
@@ -125,18 +147,26 @@ def calculate_derivative(particle, R_e, neighbor_list, typ):
             idx_j = neighbor_idx[j]
             #i[indexdx_i].append(idx_j)
             Eta = np.matmul(MinvHrs, b_data[i][j])
-            EtaDx[idx_i,idx_j] = Eta[1]
-            EtaDy[idx_i,idx_j] = Eta[2]
-            EtaDz[idx_i, idx_j] = Eta[3]
-            EtaDxx[idx_i, idx_j] = Eta[4]
-            EtaDxy[idx_i, idx_j] = Eta[5]
-            EtaDxz[idx_i, idx_j] = Eta[6]
-            EtaDyy[idx_i, idx_j] = Eta[7]
-            EtaDyz[idx_i, idx_j] = Eta[8]
-            EtaDzz[idx_i, idx_j] = Eta[9]
+            
+            if typ == 'x':
+                EtaDx[idx_i,idx_j] = Eta[1]
+                EtaDxx[idx_i, idx_j] = Eta[2]
+            elif typ == 'y':
+                EtaDy[idx_i,idx_j] = Eta[1]
+                EtaDyy[idx_i, idx_j] = Eta[2]
+            elif typ == 'z':
+                EtaDz[idx_i, idx_j] = Eta[1]
+                EtaDzz[idx_i, idx_j] = Eta[2]
+            else:
+                EtaDx[idx_i,idx_j] = Eta[1]
+                EtaDy[idx_i,idx_j] = Eta[2]
+                EtaDz[idx_i, idx_j] = Eta[3]
+                EtaDxx[idx_i, idx_j] = Eta[4]
+                EtaDyy[idx_i, idx_j] = Eta[5]
+                EtaDzz[idx_i, idx_j] = Eta[6]
             
     if typ == 'all':
-        return EtaDx, EtaDy, EtaDz, EtaDxx, EtaDxy, EtaDxz, EtaDyy, EtaDyz, EtaDzz
+        return EtaDx, EtaDy, EtaDz, EtaDxx, EtaDyy, EtaDzz
     elif typ == 'x':
         return EtaDx, EtaDxx
     elif typ == 'y':
