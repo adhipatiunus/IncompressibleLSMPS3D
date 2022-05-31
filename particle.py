8e47aa6aae15103b4c10b20a00763c6fd445fb6f
adhipatiunus <adhipatiunus@gmail.com>
2022-05-31 11:34:53 +0700

    modified class

--- particle.py
+++ particle.py
@@ -8,26 +8,21 @@ Created on Sat Jan  1 11:39:38 2022
 import numpy as np
 import numba as nb
 
-@nb.njit
-def empty_int32_list():
-    l = [nb.int32(10)]
-    l.clear()
-    return l
-
 spec = {}
 spec['x'] = nb.float64[:]
 spec['y'] = nb.float64[:]
 spec['z'] = nb.float64[:]
-spec['index'] = nb.int32
-spec['boundary'] = nb.boolean
-spec['solid'] = nb.boolean
-spec['neighbor_all'] = empty_int32_list()
-spec['neighbor_xpos'] = empty_int32_list()
-spec['neighbor_xneg'] = empty_int32_list()
-spec['neighbor_ypos'] = empty_int32_list()
-spec['neighbor_yneg'] = empty_int32_list()
-spec['neighbor_zpos'] = empty_int32_list()
-spec['neighbor_zneg'] = empty_int32_list()
+spec['index'] = nb.int32[:]
+spec['diameter'] = nb.float64[:]
+spec['boundary'] = nb.boolean[:]
+spec['solid'] = nb.boolean[:]
+spec['neighbor_all'] = nb.types.List(nb.int32)
+spec['neighbor_xpos'] = nb.types.List(nb.int32)
+spec['neighbor_xneg'] = nb.types.List(nb.int32)
+spec['neighbor_ypos'] = nb.types.List(nb.int32)
+spec['neighbor_yneg'] = nb.types.List(nb.int32)
+spec['neighbor_zpos'] = nb.types.List(nb.int32)
+spec['neighbor_zneg'] = nb.types.List(nb.int32)
 spec['n_east'] = nb.int32
 spec['n_west'] = nb.int32
 spec['n_north'] = nb.int32
@@ -35,27 +30,40 @@ spec['n_south'] = nb.int32
 spec['n_top'] = nb.int32
 spec['n_bottom'] = nb.int32
 
-@nb.jitclass(spec)
+@nb.njit
+def empty_int32_list():
+    l = [nb.int32(10)]
+    l.clear()
+    return l
+
+@nb.njit
+def empty_bool_list():
+    l = [nb.boolean(True)]
+    l.clear()
+    return l
+
+@nb.experimental.jitclass(spec)
 class Particle:
     def __init__(self):
-        self.x = np.array([])
-        self.y = np.array([])
-        self.z = np.array([])
-        self.index = np.array([])
-        self.diameter = np.array([])
-        self.boundary = np.array([])
-        self.solid = np.array([])
-        self.neighbor_all = []
-        self.neighbor_xpos = []
-        self.neighbor_xneg = []
-        self.neighbor_ypos = []
-        self.neighbor_yneg = []
-        self.neighbor_zpos = []
-        self.neighbor_zneg = []
+        self.x = np.zeros(0, dtype=np.float64)
+        self.y = np.zeros(0, dtype=np.float64)
+        self.z = np.zeros(0, dtype=np.float64)
+        self.index = np.zeros(0, dtype=np.int32)
+        self.diameter = np.zeros(0, dtype=np.float64)
+        self.boundary = np.full(0, True)
+        self.solid = np.full(0, True)
+        self.neighbor_all = empty_int32_list()
+        self.neighbor_xpos = empty_int32_list()
+        self.neighbor_xneg = empty_int32_list()
+        self.neighbor_ypos = empty_int32_list()
+        self.neighbor_yneg = empty_int32_list()
+        self.neighbor_zpos = empty_int32_list()
+        self.neighbor_zneg = empty_int32_list()
         self.n_east = 0
         self.n_west = 0
         self.n_north = 0
         self.n_south = 0
         self.n_top = 0
         self.n_bottom = 0
+        
         
